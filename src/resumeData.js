2c3c8e8ec9e78475f806aff9a21a05aeef7d3ad5
shehan2001 <shehansaverimuttu@gmail.com>
2021-05-25 23:26:39 +0530

    update

---
+++ src/resumeData.js
@@ -0,0 +1,141 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "SHEHAN ANTHONY SAVERIMUTTU",
+    "role": "Frontend Developer, Backend Developer and Android Mobile Developer ",
+    "linkedinId":"Your LinkedIn Id",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I like dabbling in various parts of front-end and back-end development and like to learn about new technologies, in my free time i like researching more about consumer technolody, Automible, sport such as Formula e and football. I do amatuer photography for fun. ",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/shehan-saverimuttu-3b35861a2",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/shehan2001",
+          "className":"fa fa-github"
+        },
+        {
+          // "name":"skype",
+          // "url":"http://twitter.com/rbhatia46",
+          // "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"I am currently a 2nd year student at Informatics Institute of Technology(IIT).",
+    "address":"Colombo, Sri Lanka",
+    "website":"https://github.com/shehan2001",
+    "education":[
+      {
+        "UniversityName":"Informatics Institute of Technology(IIT)",
+        "specialization":"Front-end, Back-end, Android Devolopement, Web Developement ",
+        "YearOfPassing":"2020",
+        "Achievements":"Batch reprenter(2020/2021), Team lead for year one web dev module group project in year one."
+      },
+      {
+        "UniversityName":" La Pettie Fleur",
+        "specialization":"Science stream",
+        "YearOfPassing":"2017 - 2020",
+        "Achievements":"House Sports captain, Wing Manager/ lead for Backstage in LPF’s public theatre productions"
+      },
+
+      {
+        "UniversityName":" Competitions",
+        "YearOfPassing":"2020 - present",
+        "Achievements":" 4th position in HAXMAS 2020, IIT HACK semi-finalist (pending results) "
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Scanwell Logistics India Private LImited",
+        "specialization":"Student Intern",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2015",
+        
+      },
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "skillsDescription":"PROGRAMMING SKILLS IN COMPARISON TO HOW CONFIDENT I AM WITH IT. (* Currenlty learning React Native and Flutter *).",
+    "skills":[
+      {
+        "skillname":"HTML"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"JavaScript"
+      },
+      {
+        "skillname":"Java"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname":"Material-UI"
+      },
+      {
+        "skillname":"Python"
+      },
+      {
+        "skillname":"Flask"
+      },
+      {
+        "skillname":"SQL-LITE"
+      },
+      {
+        "skillname":"Fire-Base"
+      },
+      {
+        "skillname":"GIT"
+      },
+      {
+        "skillname":"FIGMA"
+      },
+      {
+        "skillname":"Lightroom"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project PIKAIA",
+        "description":"project fwrwrgw",
+        "imgurl":"images/portfolio/PIKAIA_Logo.jpeg",
+        "url":"https://github.com/PIKAIA-project/PIKAIA-REST"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
