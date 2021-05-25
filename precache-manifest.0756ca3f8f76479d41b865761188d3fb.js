7ca4d2517e8266ef5093714936d91642a33c54fe
shehan2001 <shehansaverimuttu@gmail.com>
2021-05-25 21:27:05 +0530

    Updates

---
+++ precache-manifest.0756ca3f8f76479d41b865761188d3fb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a31bc17c3e463a0eebfb",
+    "url": "/Shehan-personal-website/static/css/main.240f46ed.chunk.css"
+  },
+  {
+    "revision": "a31bc17c3e463a0eebfb",
+    "url": "/Shehan-personal-website/static/js/main.a31bc17c.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/Shehan-personal-website/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "c6cd6369c446f1ecdc8b",
+    "url": "/Shehan-personal-website/static/js/runtime~main.c6cd6369.js"
+  },
+  {
+    "revision": "5e8cf9ec3c044340dc0e67f70df019fd",
+    "url": "/Shehan-personal-website/index.html"
+  }
+];
